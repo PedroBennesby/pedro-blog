ee7c579a4196560a6470facb2763f4d7c184f55a
Pedro Bennesby <pedrobennesby@gmail.com>
2022-03-30 18:08:24 -0300

    feat: :sparkles: Header

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,8 +1,25 @@
+import Link from 'next/link'
+
 function Header() {
   return (
-    <div>
-      <h1>I am a header</h1>
-    </div>
+    <header>
+      <div className="flex items-center space-x-5">
+        <Link href="/">
+          <img
+            className="h-16 w-16 cursor-pointer object-contain"
+            src="/logopng.png"
+            alt=""
+          />
+        </Link>
+        <div className="hidden items-center space-x-5 md:inline-flex">
+          <h3>About</h3>
+          <h3>Contact</h3>
+          <h3 className="rounded-full bg-green-600 px-4 py-1 text-white">
+            Follow
+          </h3>
+        </div>
+      </div>
+    </header>
   )
 }
 
