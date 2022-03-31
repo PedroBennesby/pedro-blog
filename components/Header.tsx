5f7f965561f0df25bdc2bf27285bb280f08de748
Pedro Bennesby <pedrobennesby@gmail.com>
2022-03-31 10:34:41 -0300

    feat: :sparkles: added i18n support

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,6 +1,16 @@
 import Link from 'next/link'
+import { useRouter } from 'next/router'
+import { useTranslation } from 'next-i18next'
 
 function Header() {
+  const router = useRouter()
+  const { t } = useTranslation('header')
+  const handleLocaleChange = (e: React.ChangeEvent<HTMLSelectElement>) => {
+    const value = e.target.value
+    router.push(router.route, router.asPath, {
+      locale: value,
+    })
+  }
   return (
     <header>
       <div className="flex items-center space-x-5">
@@ -12,12 +22,16 @@ function Header() {
           />
         </Link>
         <div className="hidden items-center space-x-5 md:inline-flex">
-          <h3>About</h3>
-          <h3>Contact</h3>
+          <h3>{t('about')}</h3>
+          <h3>{t('contact')}</h3>
           <h3 className="rounded-full bg-green-600 px-4 py-1 text-white">
-            Follow
+            {t('follow')}
           </h3>
         </div>
+        <select onChange={handleLocaleChange} value={router.locale}>
+          <option value="en-US">English</option>
+          <option value="pt-BR">Português</option>
+        </select>
       </div>
     </header>
   )
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,18 +1,30 @@
 import type { NextPage } from 'next'
 import Head from 'next/head'
 import Header from '../components/Header'
+import { serverSideTranslations } from 'next-i18next/serverSideTranslations'
+import { useTranslation } from 'next-i18next'
 
 const Home: NextPage = () => {
+  const { t } = useTranslation('home')
+
   return (
     <div className="">
       <Head>
-        <title>Pedro's blog</title>
+        <title>{t('name')}</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
       <Header />
     </div>
   )
+}
+
+export async function getStaticProps({ locale }: any) {
+  return {
+    props: {
+      ...(await serverSideTranslations(locale, ['common', 'home', 'header'])),
+    },
+  }
 }
 
 export default Home
