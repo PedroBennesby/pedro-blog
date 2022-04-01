6ae0b323b00d04350ce8a7c50f96f9b0da9d36bc
Pedro Bennesby <pedrobennesby@gmail.com>
2022-04-01 20:22:40 -0300

    feat: :sparkles: Create comments

--- pages/post/[slug].tsx
+++ pages/post/[slug].tsx
@@ -6,16 +6,54 @@ import Header from '../../components/Header'
 import { sanityClient, urlFor } from '../../sanity'
 import { Post } from '../../typings'
 import PortableText from 'react-portable-text'
-import { useTranslation } from 'next-i18next'
+import { useForm, SubmitHandler } from 'react-hook-form'
+import { ToastContainer, toast } from 'react-toastify'
+import 'react-toastify/dist/ReactToastify.css'
 
 interface Props {
   post: Post
 }
 
+interface IFormInput {
+  _id: string
+  name: string
+  email?: string
+  comment: string
+}
+
 function Post({ post }: Props) {
-  const { t } = useTranslation('post')
+  const [submitted, setSubmitted] = useState(false)
+  const notifySuccess = () =>
+    toast.success(locale ? 'Comment submitted' : 'Comentário enviado')
+  const notifyError = () =>
+    toast.error(
+      locale
+        ? 'Comment not submitted, try again'
+        : 'Comentário não enviado, tente novamente'
+    )
   const localeActive = useRouter().locale
   const [locale, setLocale] = useState(Boolean)
+
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<IFormInput>()
+
+  const onSubmit: SubmitHandler<IFormInput> = (data) => {
+    fetch('/api/createComment', {
+      method: 'POST',
+      body: JSON.stringify(data),
+    })
+      .then(() => {
+        setSubmitted(true)
+        notifySuccess()
+      })
+      .catch((err) => {
+        setSubmitted(false)
+        notifyError()
+      })
+  }
 
   useEffect(() => {
     if (localeActive === 'en-US') {
@@ -27,7 +65,17 @@ function Post({ post }: Props) {
   return (
     <main>
       <Header />
-
+      <ToastContainer
+        position="bottom-center"
+        autoClose={5000}
+        hideProgressBar={false}
+        newestOnTop={false}
+        closeOnClick
+        rtl={false}
+        pauseOnFocusLoss
+        draggable
+        pauseOnHover
+      />
       <img
         className="h-40 w-full object-cover"
         src={urlFor(post.mainImage).url()}
@@ -81,6 +129,95 @@ function Post({ post }: Props) {
           />
         </div>
       </article>
+      <hr className="my-5 mx-auto max-w-lg border border-yellow-500" />
+      {submitted ? (
+        <div className="my-10 mx-auto flex max-w-2xl flex-col bg-yellow-500 p-10 text-white">
+          <h3 className="text-3xl font-bold">
+            {locale
+              ? 'Thank you for submitting your comment!'
+              : 'Obrigado por enviar seu comentário!'}
+          </h3>
+          <p>
+            {locale
+              ? 'Once it has been approved, it will appear below'
+              : 'Assim que ele for aprovado, irá aparecer aqui embaixo'}
+          </p>
+        </div>
+      ) : (
+        <form
+          onSubmit={handleSubmit(onSubmit)}
+          className="my-10 mx-auto mb-10 flex max-w-2xl flex-col p-5"
+        >
+          <h3 className="text-sm text-yellow-500">
+            {locale ? 'Enjoyed this article?' : 'Gostou desse artigo?'}
+          </h3>
+
+          <h4 className="text-3xl font-bold">
+            {locale ? 'Leave a comment below!' : 'Deixe um comentário abaixo!'}
+          </h4>
+
+          <hr className="mt-2 py-3" />
+
+          <input
+            {...register('_id')}
+            type="hidden"
+            name="_id"
+            value={post._id}
+          />
+
+          <label className="mb-5 block">
+            <span className="text-gray-700">{locale ? 'Name' : 'Nome'}</span>
+            <input
+              {...register('name', { required: true })}
+              className="form-input mt-1 block w-full rounded border py-2 px-3 shadow "
+              placeholder="John Doe"
+              type="text"
+            />
+          </label>
+
+          <label className="mb-5 block">
+            <span className="text-gray-700">Email</span>
+            <input
+              {...register('email')}
+              className="form-input shado mt-1 block w-full rounded border py-2 px-3"
+              placeholder="John Doe"
+              type="email"
+            />
+          </label>
+
+          <label className="mb-5 block">
+            <span className="text-gray-700">
+              {locale ? 'Comment' : 'Comentário'}
+            </span>
+            <textarea
+              {...register('comment', { required: true })}
+              className="form-textarea mt-1 block w-full rounded border py-2 px-3 shadow"
+              placeholder="John Doe"
+              rows={8}
+            />
+          </label>
+          <div className="flex flex-col p-5">
+            {errors.name && (
+              <span className="text-red-500">
+                {locale
+                  ? '- The Name Field is required'
+                  : '- O campo Nome é obrigatório'}
+              </span>
+            )}
+            {errors.comment && (
+              <span className="text-red-500">
+                {locale
+                  ? '- The Comment Field is required'
+                  : '- O campo Comentário é obrigatório'}
+              </span>
+            )}
+          </div>
+          <input
+            type="submit"
+            className="focus:shadow-outline cursor-pointer rounded bg-yellow-500 py-2 px-4 font-bold text-white shadow transition duration-300 ease-in-out hover:bg-yellow-400 focus:outline-none"
+          />
+        </form>
+      )}
     </main>
   )
 }
---
+++ pedroblog/schemas/comment.js
@@ -0,0 +1,30 @@
+export default {
+  name: 'comment',
+  type: 'document',
+  title: 'Comment',
+  fields: [
+    {
+      name: 'name',
+      type: 'string',
+    },
+    {
+      title: 'Approved',
+      name: 'approved',
+      type: 'boolean',
+      description: 'Approved comments are visible to the public.',
+    },
+    {
+      name: 'email',
+      type: 'string',
+    },
+    {
+      name: 'comment',
+      type: 'text',
+    },
+    {
+      name: 'post',
+      type: 'reference',
+      to: [{ type: 'post' }],
+    },
+  ],
+}
