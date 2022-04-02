92bcf05ccc159b5ebf464b52f44b61d84cad6685
Pedro Bennesby <pedrobennesby@gmail.com>
2022-04-01 21:03:54 -0300

    fix: :bug: Update i18n

--- next-i18next.config.js
+++ next-i18next.config.js
@@ -1,7 +1,9 @@
+const path = require('path')
 module.exports = {
   i18n: {
     defaultLocale: 'en-US',
     locales: ['en-US', 'pt-BR'],
-    localePath: './locales',
+    localePath: path.resolve('./locales'),
+    localeDetection: false,
   },
 }
